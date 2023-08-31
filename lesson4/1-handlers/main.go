9d4e31d3171af8a2d244443cd3ced102b07c4207
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-08-31 14:47:49 +0530

    lesson 4: 1: handlers 1

---
+++ lesson4/1-handlers/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1> Contact Info </h1>")
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/contact", contact)
+	fmt.Println("Server is starting...")
+	http.ListenAndServe(":3000", nil)
+}
